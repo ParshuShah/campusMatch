590fb7d79d7b0e508d05cd9e160be3b7d920e79d
ParshuShah <learnerparshuram@gmail.com>
2025-06-25 14:55:26 +0530

    chhnaged the backend cors url

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
