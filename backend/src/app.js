4fc37b63e3a98a0b67456548d481b9b305d1f187
ParshuShah <learnerparshuram@gmail.com>
2025-07-10 16:50:03 +0530

    cors policy updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 // require("./utils/cronjob");  not need it is for the email thing
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://13.53.212.125"],
+    origin: ["http://localhost:5173", "http://13.53.212.125", "https://campusmatch-backend.onrender.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
