e5e52bd5323087bb4c3b45e55b135667f357a371
ParshuShah <learnerparshuram@gmail.com>
2025-07-10 17:11:35 +0530

    cookie this corrected

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -8,70 +8,81 @@ const bcrypt = require("bcrypt");
 //making an API to add the data into the DB.
 authRouter.post("/signup", async (req, res) => {
 
-    try {
-        //validation of data
-        validateSignUpData(req);
+  try {
+    //validation of data
+    validateSignUpData(req);
 
-        const { firstName, lastName, emailId, password } = req.body;
-        //Encrypt the password
-        const passwordHash = await bcrypt.hash(password, 10);
-        // console.log(passwordHash)
+    const { firstName, lastName, emailId, password } = req.body;
+    //Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    // console.log(passwordHash)
 
-        //Creating a new Instance of the User Model
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash,
-        });
+    //Creating a new Instance of the User Model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
 
 
-        const savedUser = await user.save();
-        const token = await savedUser.getJWT();
-    
-        res.cookie("token", token, {
-          expires: new Date(Date.now() + 8 * 3600000),
-        });
-    
-        res.json({ message: "User Added successfully!", data: savedUser });
-      } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-      }
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,           // IMPORTANT for HTTPS
+      sameSite: "None",       // IMPORTANT for cross-origin
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.json({ message: "User Added successfully!", data: savedUser });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
 
 //Login API
 authRouter.post("/login", async (req, res) => {
-    try {
-        const { emailId, password } = req.body;
-        //second emailId is the email by req.body means by the user;
-        const user = await User.findOne({ emailId: emailId });
-        if (!user) {
-            throw new Error("Invaid Credentials");
-        }
-        //user.password is the encrypt password inside the db
-        const isPasswordValid = await user.validatePassword(password);
-
-        if (isPasswordValid) {
-
-            //create a JWT token
-            const token = await user.getJWT();
-            //Add the token to cookie and send the response back to the user
-            res.cookie("token", token);
-            res.send(user);
-        } else {
-            throw new Error("Invalid Credentials");
-        }
-
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message)
+  try {
+    const { emailId, password } = req.body;
+    //second emailId is the email by req.body means by the user;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invaid Credentials");
     }
+    //user.password is the encrypt password inside the db
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+
+      //create a JWT token
+      const token = await user.getJWT();
+      //Add the token to cookie and send the response back to the user
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        expires: new Date(Date.now() + 8 * 3600000), // optional
+      });
+      res.send(user);
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message)
+  }
 })
 //Logout API
 authRouter.post("/logout", async (req, res) => {
-    res.cookie("token", null, {
-      expires: new Date(Date.now()),
-    });
-    res.send("Logout Successful!!");
+  res.cookie("token", null, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    expires: new Date(Date.now()),
   });
+  res.send("Logout Successful!!");
+});
 
 module.exports = authRouter;
